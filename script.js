94ae5de84d9eb4bd535e1c54524545225271817a
Caleb Mahala <31549512+calebgmahala@users.noreply.github.com>
2018-12-07 20:25:25 -0500

    edited console error

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
 function slide(slider) {
 	var theSlider = '.'+slider;
-	var offsetT = $(theSlider).offset().top - $(document).scrollTop();
-	if (offsetT < 0 && offsetT > -80) {
-		$(theSlider).css('opacity', 1-(-offsetT / 80));
-		$(theSlider).css('padding-left', (20-(-offsetT * .25)+'%'));
-	} else if (offsetT > 0) {
-		$(theSlider).css('opacity', 1);
-		$(theSlider).css('padding-left', 20+'%');
-	} else {
-		$(theSlider).css('opacity', 0);
-		$(theSlider).css('padding-left', 0+'%');
-	}
-	if (offsetT < -150 && offsetT > -300) {
-		$(".t"+slider).css('opacity', 1-(-(offsetT+150) / 150));
-	} else if (offsetT > -150) {
-		$(".t"+slider).css('opacity', 1);
-	} else {
-		$(".t"+slider).css('opacity', 0);
+	if ($(theSlider).offset() != undefined) {
+		var offsetT = $(theSlider).offset().top - $(document).scrollTop();
+		if (offsetT < 0 && offsetT > -80) {
+			$(theSlider).css('opacity', 1-(-offsetT / 80));
+			$(theSlider).css('padding-left', (20-(-offsetT * .25)+'%'));
+		} else if (offsetT > 0) {
+			$(theSlider).css('opacity', 1);
+			$(theSlider).css('padding-left', 20+'%');
+		} else {
+			$(theSlider).css('opacity', 0);
+			$(theSlider).css('padding-left', 0+'%');
+		}
+		if (offsetT < -150 && offsetT > -300) {
+			$(".t"+slider).css('opacity', 1-(-(offsetT+150) / 150));
+		} else if (offsetT > -150) {
+			$(".t"+slider).css('opacity', 1);
+		} else {
+			$(".t"+slider).css('opacity', 0);
+		}
 	}
 }
 function ilove(thing) {
